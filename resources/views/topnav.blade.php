33228c430592fa18ef17fbb5d95c710ddd7aedfd
anthonyPlexuss <anthony.shayesteh@plexuss.com>
2016-04-19 18:26:13 -0700

    wip

--- resources/views/topnav.blade.php
+++ resources/views/topnav.blade.php
@@ -9,7 +9,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#"><img src="images/logo.png" alt="logo" style="padding-top: 1em;"></a>
+				<a class="navbar-brand" href="/"><img src="images/logo.png" alt="logo" style="padding-top: 1em;"></a>
 			</div>
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
